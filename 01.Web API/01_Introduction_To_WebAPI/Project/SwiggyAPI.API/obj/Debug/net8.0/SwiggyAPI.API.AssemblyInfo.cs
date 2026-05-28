ab6b11594920d5171a52ec48abccb455eee08cb7
Thillaishanmugam1998 <shansenthil72@gmail.com>
2026-05-28 19:09:18 +0530

    Fixed

--- 01.Web API/01_Introduction_To_WebAPI/Project/SwiggyAPI.API/obj/Debug/net8.0/SwiggyAPI.API.AssemblyInfo.cs
+++ 01.Web API/01_Introduction_To_WebAPI/Project/SwiggyAPI.API/obj/Debug/net8.0/SwiggyAPI.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwiggyAPI.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fae59509b1e68c0b27ce6e61c2e4849b49c4fc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67bba6b80e1b818cabcd23f55d3201ce0d9d9ae7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwiggyAPI.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwiggyAPI.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
